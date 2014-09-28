5f2e13a80cf71d108498272caad88a2e321d6747
Lasean2 <lcbreploid@msn.com>
2014-09-27 23:03:39 -0400

    Projectile Update to MainClass
    Projectile class is called upon in the Run() method to update position.

--- Hogienator/src/hoagienator/MainClass.java
+++ Hogienator/src/hoagienator/MainClass.java
@@ -1,5 +1,80 @@
 package hoagienator;
 
-public class MainClass {
+import java.applet.Applet;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+public class MainClass extends Applet implements Runnable, KeyListener{
+	
+	@Override
+    public void init() {
+        // TODO Auto-generated method stub
+        super.init();
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        super.start();
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        super.stop();
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+        super.destroy();
+    }
+	
+	/**
+	 * Main Game loop that updates all objects on every iteration. ie. bullet position, character position
+	 */ 
+	@Override
+	public void run() {
+		
+		while (true) {
+			//Removes offscreen projectiles from memory.
+			//REPLACE 'SHOOTER' WITH REAL CLASS NAME.
+			ArrayList<Projectile> projectiles = SHOOTER.getProjectiles();
+			for(int i=0; i<projectiles.size();i++){
+				Projectile p = (Projectile) projectiles.get(i);
+				if(p.isVisible() == true){
+					p.update();
+				}else {
+					projectiles.remove(i);
+				}
+			}
+			
+			//Sleep for 17 milliseconds = 60 updates per second
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
