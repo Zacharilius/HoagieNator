9142b15841741b204c4982fc9a251d9600d30462
Lasean2 <lcbreploid@msn.com>
2014-09-27 22:08:04 -0400

    Projectile v1
    Added all default classes from working through tutorial site.

--- Hogienator/src/hoagienator/Projectile.java
+++ Hogienator/src/hoagienator/Projectile.java
@@ -1,5 +1,61 @@
 package hoagienator;
 
 public class Projectile {
+	private int x,y;
+	private int speedX; //Speed of projectile
+	private boolean visible;
+	
+	/**
+	 * Default Constructor.
+	 * @param startX - starting X coordinate of projectile.
+	 * @param startY - starting Y coordinate of projectile.
+	 */
+	public Projectile(int startX,int startY) {
+		x = startX;
+		y = startY;
+		speedX = 7; 
+		visible = true;
+	}
+	
+	/**
+	 * Called to update the position of the projectile(x) and make offscreen projectiles invisible.
+	 */
+	public void update() {
+		x += speedX;
+		if(x > 800){
+			visible = false;
+		}
+	}
 
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getSpeedX() {
+		return speedX;
+	}
+
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public void setSpeedX(int speedX) {
+		this.speedX = speedX;
+	}
+
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
 }
